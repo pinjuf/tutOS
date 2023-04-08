587869f03cf512dcceb24bc6be539f44c3de47f8
pinjuf <paul.denise9110@gmail.com>
2023-04-08 18:31:53 +0200

    Introduce 10_KeyboardDriver

---
+++ 10_KeyboardDriver/README.txt
@@ -0,0 +1,11 @@
+Chapter 10 - Keyboard driver
+
+Everything has IO. Life, the universe, and
+your PC too. And sometimes, they just don't "O"
+42, they also need some "I". For this, we will
+use the PS/2 interface. First, we set up the 8042
+PS/2 controller, and then, we can just use keyboard
+interrupts! Later on, we should be able to easily add
+mouse support!
+
+FAQ:
---
+++ 10_KeyboardDriver/kernel/main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include "paging.h"
+#include "vga.h"
+#include "util.h"
+#include "idt.h"
+#include "gdt.h"
+#include "mm.h"
+#include "pic.h"
+
+void _kmain() {
+    // Set up our stack
+    asm volatile(" \
+        mov $0x120000, %rsp; \
+        mov %rsp, %rbp; \
+            "); 
+
+    // Set up VGA
+    vga_enable_cursor(0, 16);
+    vga_get_cursor();
+
+    // I really like cool sounding logs
+    kputs("KRN OK\n");
+
+    init_kgdt();
+    kputs("GDT OK\n");
+
+    init_idt();
+    kputs("IDT OK\n");
+
+    init_paging();
+    kputs("PGN OK\n");
+
+    init_mm();
+    kputs("MM  OK\n");
+
+    init_pic();
+    kputs("PIC OK\n");
+
+    init_pit0(PIT0_FREQ); // Interrupt every 4ms
+    kputs("PIT OK\n");
+
+    init_8042ps2();
+    kputs("KBD OK\n");
+
+    kputs("KRN MN\n");
+
+    sti;
+
+    kputs("KRN DN\n");
+    while (1);
+}
