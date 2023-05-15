4e7f8468b36b626df5195213e9b8d21c67c1e98e
pinjuf <paul.denise9110@gmail.com>
2023-05-15 13:15:46 +0200

    Introduce ELF loader

---
+++ 21_ELF/kernel/vesa.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "types.h"
+#include "psf.h"
+
+typedef struct vbe_info_t {
+	char signature[4];
+	uint16_t version;
+	uint32_t oem;
+	uint32_t capabilities;
+	uint32_t video_modes;
+	uint16_t video_memory;
+	uint16_t software_rev;
+	uint32_t vendor;
+	uint32_t product_name;
+	uint32_t product_rev;
+	char reserved[222];
+	char oem_data[256];
+} __attribute__((packed)) vbe_info_t;
+
+typedef struct vbe_mode_info_t {
+    uint16_t attributes;
+	uint8_t window_a;
+	uint8_t window_b;
+	uint16_t granularity;
+	uint16_t window_size;
+	uint16_t segment_a;
+	uint16_t segment_b;
+	uint32_t win_func_ptr;
+	uint16_t pitch;
+	uint16_t width;
+	uint16_t height;
+	uint8_t w_char;
+	uint8_t y_char;
+	uint8_t planes;
+	uint8_t bpp;
+	uint8_t banks;
+	uint8_t memory_model;
+	uint8_t bank_size;
+	uint8_t image_pages;
+	uint8_t reserved0;
+
+	uint8_t red_mask;
+	uint8_t red_position;
+	uint8_t green_mask;
+	uint8_t green_position;
+	uint8_t blue_mask;
+	uint8_t blue_position;
+	uint8_t reserved_mask;
+	uint8_t reserved_position;
+	uint8_t direct_color_attributes;
+
+	uint32_t framebuffer;
+	uint32_t off_screen_mem_off;
+	uint16_t off_screen_mem_size;
+	uint8_t reserved1[206];
+} __attribute__((packed)) vbe_mode_info_t;
+
+#define VESA_VIRT_FB 0xFFFF800000000000 // TODO: Find a better position
+
+void init_vesa();
+
+// We assume a 32-bpp framebuffer with 0x00RRGGBB format
+typedef uint32_t rgb32_t;
+#define RGB32(r, g, b) ((rgb32_t)(((r) << 16) | ((g) << 8) | (b)))
+#define SET_PIXEL(x, y, color) *(uint32_t*)((size_t)VESA_VIRT_FB + (y) * bpob->vbe_mode_info.pitch + (x) * sizeof(uint32_t)) = (color)
+#define GET_PIXEL(x, y) (*(uint32_t*)((size_t)VESA_VIRT_FB + (y) * bpob->vbe_mode_info.pitch + (x) * sizeof(uint32_t))) // TODO: Add double buffering
+
+void vesa_clear(rgb32_t c);
+void vesa_drawrect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, rgb32_t c);
+void vesa_drawcircle(uint32_t x, uint32_t y, int r, rgb32_t c);
+void vesa_scrolldown(uint32_t n);
+
+extern uint32_t vheight, vwidth;
+extern psf2_header_t * vfont;
+extern rgb32_t vfont_fg, vfont_bg;
+extern bool vesa_ready;
+extern uint32_t vesa_x, vesa_y;
+
+void vesa_putc(char c);
+void vesa_puts(char * s);
---
+++ 21_ELF/kernel/vfs.c
@@ -0,0 +1,94 @@
+#include "vfs.h"
+#include "mm.h"
+#include "util.h"
+
+mountpoint_t * mountpoints;
+
+void init_vfs() {
+    mountpoints = (mountpoint_t *) kcalloc(sizeof(mountpoint_t) * MOUNTPOINTS_N);
+
+    mountpoints[0].type = FS_EXT2;
+    mountpoints[0].path = "/";
+    mountpoints[0].p    = get_part(1, 0);
+
+    for (size_t i = 0; i < MOUNTPOINTS_N; i++) {
+        if (mountpoints[i].type == FS_UNKN)
+            continue;
+        mountpoints[i].internal_fs = FILESYSTEMS[mountpoints[i].type].get_fs(mountpoints[i].p);
+    }
+}
+
+filehandle_t * kopen(char * p, enum FILEMODE mode) {
+
+    // Get the string with the longest starting match
+    // TODO: Shorten "x/y/../z" to "x/z"
+
+    // We might do "unsanitary" stuff to the string
+    char * path = kmalloc(strlen(p)+1);
+    memcpy(path, p, strlen(p)+1);
+
+    if (strlen(path)>1 && path[strlen(path)-1] == DIRSEP) { // Remove ending "/" if the path is not "/" (root dir)
+        path[strlen(path)-1] = 0;
+    }
+
+    size_t mountpoint = -1;
+    size_t length = 0;
+    for (size_t m = 0; m < MOUNTPOINTS_N; m++) {
+        size_t match = strmatchstart(mountpoints[m].path, path);
+        if (match > length && strlen(mountpoints[m].path) == match) {
+            length = match;
+            mountpoint = m;
+        }
+    }
+
+    if (mountpoint == (size_t)-1) {
+        kwarn(__FILE__,__func__,"mountpoint not found");
+
+        kfree(path);
+
+        return NULL;
+    }
+
+    if (FILESYSTEMS[mountpoints[mountpoint].type].get_filehandle == NULL) {
+        kwarn(__FILE__,__func__,"no driver support");
+
+        kfree(path);
+
+        return NULL;
+    }
+
+    filehandle_t * out = FILESYSTEMS[mountpoints[mountpoint].type].get_filehandle(mountpoints[mountpoint].internal_fs, path + length, mode);
+
+    if (out) {
+        out->mountpoint = mountpoint;
+        out->mode = mode;
+    }
+
+    kfree(path);
+
+    return out;
+}
+
+void kclose(filehandle_t * f) {
+    if (FILESYSTEMS[mountpoints[f->mountpoint].type].close_filehandle == NULL) {
+        kwarn(__FILE__,__func__,"no driver support");
+    }
+
+    FILESYSTEMS[mountpoints[f->mountpoint].type].close_filehandle(f);
+}
+
+size_t kread(filehandle_t * f, void * buf, size_t count) {
+    if (FILESYSTEMS[mountpoints[f->mountpoint].type].read_file == NULL) {
+        kwarn(__FILE__,__func__,"no driver support");
+    }
+
+    return FILESYSTEMS[mountpoints[f->mountpoint].type].read_file(f, buf, count);
+}
+
+size_t kwrite(filehandle_t * f, void * buf, size_t count) {
+    if (FILESYSTEMS[mountpoints[f->mountpoint].type].read_file == NULL) {
+        kwarn(__FILE__,__func__,"no driver support");
+    }
+
+    return FILESYSTEMS[mountpoints[f->mountpoint].type].write_file(f, buf, count);
+}
