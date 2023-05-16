a261cc40b94c5892a9d24944defe3ca332bc78c6
pinjuf <paul.denise9110@gmail.com>
2023-05-16 21:32:37 +0200

    i can feel enivitable failure coming

--- 21_ELF/userland/src/test.c
+++ 21_ELF/userland/src/test.c
@@ -3,6 +3,6 @@
 
 extern uint64_t syscall(uint64_t syscall_num, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5);
 
-void _start() {
+int main() {
     while (1) syscall(1, 1, 1, 1, 1, 1, 1);
 }
