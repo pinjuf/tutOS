0c6564483dc2eae428e0e662eb28055873967c6e
pinjuf <paul.denise9110@gmail.com>
2023-05-31 13:39:54 +0200

    Add /bin/ls

--- 28_Args/userland/lib/unistd.h
+++ 28_Args/userland/lib/unistd.h
@@ -21,6 +21,13 @@ enum SEEKMODE {
     SEEK_END,
 };
 
+typedef struct dirent {
+    enum FILETYPE d_type;
+    size_t d_size;
+    uint8_t d_namlen;
+    char d_name[256];
+} dirent_t;
+
 // I know, I know, don't judge me...
 typedef void FILE;
 typedef void DIR;
