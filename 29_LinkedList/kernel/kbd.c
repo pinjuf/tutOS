5014113e8bf2e3860c5a3d727f4eb5d60c942050
pinjuf <paul.denise9110@gmail.com>
2023-06-01 16:34:53 +0200

    Introduce linked lists

---
+++ 29_LinkedList/kernel/kbd.c
@@ -0,0 +1,66 @@
+#include "kbd.h"
+
+#include "util.h"
+
+bool mouse_left   = false;
+bool mouse_middle = false;
+bool mouse_right  = false;
+uint8_t mouse_scroll = PS2_MOUSE_SCRL_NONE;
+
+float mouse_x = 0;
+float mouse_y = 0;
+
+size_t mouse_xlim = 800; // These values get changed on VESA init
+size_t mouse_ylim = 600;
+
+uint8_t * kbd_bitmap;
+
+char kbd_last_ascii;
+uint16_t kbd_last_scancode;
+
+void kbd_setkey(uint16_t key, bool status) {
+    uint16_t i = key/8;
+    uint8_t  j = key%8;
+
+    if (status) {
+        kbd_bitmap[i] |= 1<<j;
+    } else {
+        kbd_bitmap[i] &= ~(1<<j);
+    }
+}
+
+bool kbd_getkey(uint16_t key) {
+    uint16_t i = key/8;
+    uint8_t  j = key%8;
+
+    return (kbd_bitmap[i] & (1<<j)) != 0;
+}
+
+char scancode_to_ascii(uint8_t s) {
+    if (s*2 >= sizeof(KBD_DEFAULT_TRANSLATOR))
+        return 0;
+    return KBD_DEFAULT_TRANSLATOR[s * 2];
+}
+
+char scancode_shift_to_ascii(uint8_t s) {
+    if (s*2 >= sizeof(KBD_DEFAULT_TRANSLATOR))
+        return 0;
+    return KBD_DEFAULT_TRANSLATOR[s * 2 + 1];
+}
+
+// Should it be necessary to get the last key WITHOUT blocking, you can just read kbd_last_ascii
+__attribute__((optimize("O0"))) // GCC will transform "kbd_last_ascii==0" into "true"
+char kbd_get_last_ascii() {
+    while (kbd_last_ascii == 0);
+    char o = kbd_last_ascii;
+    kbd_last_ascii = 0;
+    return o;
+}
+
+__attribute__((optimize("O0")))
+uint16_t kbd_get_last_scancode() {
+    while (kbd_last_scancode == 0);
+    uint16_t o = kbd_last_scancode;
+    kbd_last_scancode = 0;
+    return o;
+}
