8ad6080211fe2aa4622ec677492d6da58f87bef3
pinjuf <paul.denise9110@gmail.com>
2023-07-14 23:08:36 +0200

    testing is for loosers

--- 31_Signals/userland/lib/string.c
+++ 31_Signals/userland/lib/string.c
@@ -46,3 +46,47 @@ int strncmp(char * s1, char * s2, size_t n) {
 
     return *s1 - *s2;
 }
+
+// Shamelessly stolen from stackoverflow 190229
+void itoa(int n, char s[], uint8_t base) {
+     int i, sign;
+
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = "0123456789ABCDEF" [n % base];   /* get next digit */
+     } while ((n /= base) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+}
+
+void reverse(char s[]) {
+    int i, j;
+    char c;
+
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+       s[j] = c;
+    }
+}
+
+int atoi(char * s, uint8_t base) {
+    int n = 0;
+    int i = 0;
+    int sign = 1;
+
+    if (s[0] == '-') {
+        sign = -1;
+        i++;
+    }
+
+    for (; s[i] != '\0'; i++) {
+        n = n * base + s[i] - '0';
+    }
+
+    return sign * n;
+}
--- 31_Signals/userland/lib/string.h
+++ 31_Signals/userland/lib/string.h
@@ -9,3 +9,8 @@ size_t strlen(char * s);
 int strcmp(char * s1, char * s2);
 int strncmp(char * s1, char * s2, size_t n);
 char * strcpy(char * dest, char * src);
+
+void itoa(int n, char s[], uint8_t base);
+void reverse(char s[]);
+
+int atoi(char * s, uint8_t base);
