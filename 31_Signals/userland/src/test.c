397448d43c5340b5c2f129591b95c5bba2aca50b
pinjuf <paul.denise9110@gmail.com>
2023-07-20 22:44:46 +0200

    tests

--- 31_Signals/userland/src/test.c
+++ 31_Signals/userland/src/test.c
@@ -3,11 +3,25 @@
 #include "string.h"
 #include "unistd.h"
 
+void sighandle(int signum) {
+    if (signum == SIGSTOP) {
+        puts("I was stopped!\n");
+    } else if (signum == SIGCONT) {
+        puts("I was continued!\n");
+    }
+
+    sigreturn();
+}
+
 int main(int argc, char * argv[]) {
+
+    signal(SIGSTOP, sighandle);
+    signal(SIGCONT, sighandle);
+
     puts("Hello from test program!\n");
 
     pit_msleep(5000);
-    asm ("cli"); // Causes a #GP
+    //asm ("cli"); // Causes a #GP
     pit_msleep(5000);
 
     puts("Bye from test program!\n");
