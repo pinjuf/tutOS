ad09d03ed44138edb6d4e90900f9f145162c1730
pinjuf <paul.denise9110@gmail.com>
2023-07-22 20:52:04 +0200

    oops

---
+++ 31_Signals/lib/unistd.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "types.h"
+
+enum FILETYPE {
+    FILE_UNKN,
+    FILE_REG,
+    FILE_DIR,
+    FILE_BLK,
+    FILE_DEV,
+};
+
+typedef uint16_t mode_t;
+#define O_RDONLY 1
+#define O_WRONLY 2
+#define O_RDWR   3
+
+enum SEEKMODE {
+    SEEK_SET = 0,
+    SEEK_CUR,
+    SEEK_END,
+};
+
+typedef struct dirent {
+    enum FILETYPE d_type;
+    size_t d_size;
+    uint8_t d_namlen;
+    char d_name[256];
+} dirent;
+
+typedef struct stat {
+    size_t st_dev; // device, a.k.a. mountpoint #N
+    uint8_t st_mode; // only FILETYPE here
+    size_t st_size;
+} stat;
+
+// I know, I know, don't judge me...
+typedef void FILE;
+typedef void DIR;
+typedef uint16_t pid_t;
+
