5fd2a83e350c72e7376e4c7000d86554447f9df1
pinjuf <paul.denise9110@gmail.com>
2023-08-29 17:19:38 +0200

    i accept failure

--- 39_Multiprocessing/userland/lib/stdlib.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "types.h"
-
-void * malloc(size_t n);
-void free(void * a);
--- 39_Multiprocessing/userland/src/clear.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char * argv[]) {
-    puts("\033[H\033[J");
-    return 0;
-}
